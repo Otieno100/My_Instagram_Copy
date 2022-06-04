92ad2851832de077ba0fff7eb67b1ea9b163ad7f
Otieno100 <brian.masafu@student.moringaschool.com>
2022-06-04 23:22:12 +0300

    added welcome views

--- instagram/views.py
+++ instagram/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+import datetime as dt
+from .models import  Image
 
 # Create your views here.
 def index(request):
@@ -7,3 +9,7 @@ def index(request):
 
 
 
+def profile(request):
+    date = dt.date.today()
+    profile = Image.profile()
+    return render(request, 'instagram/profile.html', {"date": date,"post":profile})
